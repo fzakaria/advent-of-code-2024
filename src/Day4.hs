88340f347037d075608e06928dbc746ad99ff19c
Farid Zakaria <farid.m.zakaria@gmail.com>
2024-12-04 21:23:21 -0800

    Day 2 part 2

--- src/Day4.hs
+++ src/Day4.hs
@@ -39,6 +39,32 @@ xmasOccurrences grid = sum [helper (x, y) "" grid (dx, dy) | (x, y) <- allStarti
             let char = p !! x !! y
                 w' = w ++ [char]
             in helper (x + dx, y + dy) w' p (dx, dy)
+
+-- We want to count all the occurrence of the word MAS in the grid
+-- that cross each other.
+-- My plan is to iterate through all the A's and see if they have MAS
+-- that coss each other.
+crossMasOccurrences :: [[Char]] -> Int
+crossMasOccurrences grid = sum [helper (x, y) grid | (x, y) <- allStartingPoints]
+    where
+        rows = length grid
+        cols = if null grid then 0 else length (head grid)
+        allStartingPoints = [(x, y) | x <- [0..rows - 1], y <- [0..cols - 1], grid !! x !! y == 'A']
+        -- Helper function to check if a string matches "MAS" or "SAM"
+        isAllowed :: String -> Bool
+        isAllowed word = word == "MAS" || word == "SAM"
+        -- for each starting point, just check if MAS crosses each other
+        helper :: (Int, Int) -> [[Char]] -> Int
+        helper (x, y) p
+            -- We need at least one space since A is in the middle
+            | x <= 0 || y <= 0 || x >= length p - 1|| y >= length (p !! x) - 1 = 0
+            | otherwise =
+            let a = p !! x !! y
+                mas1 = [(p !! (x - 1))!! (y - 1), a, (p !! (x + 1)) !! (y + 1)]
+                mas2 = [(p !! (x + 1))!! (y - 1), a, (p !! (x - 1)) !! (y + 1)] 
+                -- the words can be reversed so check for that too
+            in if isAllowed mas1 && isAllowed mas2 then 1 else 0
+    
 main :: IO ()
 main = do
   input <- readFile "input/Day4.txt"
@@ -46,3 +72,4 @@ main = do
     Left err -> putStrLn $ errorBundlePretty err
     Right p -> do
       print $ xmasOccurrences p
+      print $ crossMasOccurrences p
