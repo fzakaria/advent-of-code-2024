118a9cfaf8f90d1436fddcd79e4bd8da894ab9df
Farid Zakaria <farid.m.zakaria@gmail.com>
2024-12-04 20:44:14 -0800

    Simplify part 1 Day 4

--- src/Day4.hs
+++ src/Day4.hs
@@ -20,38 +20,25 @@ row = many letter
 puzzle :: Parser [[Char]]
 puzzle = row `sepBy` newline
 
-data Direction = Up | Down | L | R | UpLeft | UpRight | DownLeft | DownRight deriving (Eq, Show)
-
-xmasOccurrences :: [[Char]] -> Integer
-xmasOccurrences grid = sum [helper (x, y) "" grid dir | (x, y) <- allStartingPoints, dir <- allDirections]
+xmasOccurrences :: [[Char]] -> Int
+xmasOccurrences grid = sum [helper (x, y) "" grid (dx, dy) | (x, y) <- allStartingPoints, (dx, dy) <- allDirections]
     where
         rows = length grid
         cols = if null grid then 0 else length (head grid)
         allStartingPoints = [(x, y) | x <- [0..rows - 1], y <- [0..cols - 1], grid !! x !! y == 'X']
         -- All possible directions
-        allDirections = [Up, Down, L, R, UpLeft, UpRight, DownLeft, DownRight]
+        allDirections = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
 
-        helper :: (Int, Int) -> String -> [[Char]] -> Direction -> Integer
+        helper :: (Int, Int) -> String -> [[Char]] -> (Int, Int) -> Int
         helper _ "XMAS" _ _ = 1
-        helper (x, y) w p dir
+        helper (x, y) w p (dx, dy)
             | x < 0 || y < 0 || x >= length p || y >= length (p !! x) = 0
             -- if the current word isn't a prefix of XMAS, we can't continue
             | not (w `isPrefixOf` "XMAS") = 0
             | otherwise =
             let char = p !! x !! y
                 w' = w ++ [char]
-                (dx, dy) = directionDelta dir
-            in helper (x + dx, y + dy) w' p dir
-        -- Compute (dx, dy) for each direction
-        directionDelta :: Direction -> (Int, Int)
-        directionDelta Up        = (-1, 0)
-        directionDelta Down      = (1, 0)
-        directionDelta L      = (0, -1)
-        directionDelta R     = (0, 1)
-        directionDelta UpLeft    = (-1, -1)
-        directionDelta UpRight   = (-1, 1)
-        directionDelta DownLeft  = (1, -1)
-        directionDelta DownRight = (1, 1)
+            in helper (x + dx, y + dy) w' p (dx, dy)
 main :: IO ()
 main = do
   input <- readFile "input/Day4.txt"
